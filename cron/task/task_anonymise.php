b13a1ab16faecff7d4b39b58b7018d5887e049d5
Christian Schnegelberger <mail@crizzo.de>
2019-09-25 19:16:20 +0200

    Avoid that already overwritten entry get the anonymised IP

--- cron/task/task_anonymise.php
+++ cron/task/task_anonymise.php
@@ -20,42 +20,44 @@ class task_anonymise
 	public function anonymise_ips($time_run, $db)
 	{
 		// postings
-		$sql = 'UPDATE ' . POSTS_TABLE . "
-			SET poster_ip = '127.0.0.1'
-			WHERE post_time < " . (int)$time_run;
+		$sql = 'UPDATE ' . POSTS_TABLE . '
+			SET poster_ip = "127.0.0.1"
+			WHERE post_time < ' . (int)$time_run . ' AND poster_ip <> "127.0.0.1"';
 		$db->sql_query_limit($sql, 200);
 
 		// private messages
-		$sql = 'UPDATE ' . PRIVMSGS_TABLE . "
-			SET author_ip = '127.0.0.1'
-			WHERE message_time < " . (int)$time_run;
+		$sql = 'UPDATE ' . PRIVMSGS_TABLE . '
+			SET author_ip = "127.0.0.1"
+			WHERE message_time < ' . (int)$time_run . ' AND author_ip <> "127.0.0.1"';
 		$db->sql_query_limit($sql, 200);
 
 		// user
-		$sql = 'UPDATE ' . USERS_TABLE . "
-			SET user_ip = '127.0.0.1'
-			WHERE user_regdate < " . (int)$time_run;
+		$sql = 'UPDATE ' . USERS_TABLE . '
+			SET user_ip = "127.0.0.1"
+			WHERE user_regdate < ' . (int)$time_run . ' AND user_ip <> "127.0.0.1"';
 		$db->sql_query_limit($sql, 200);
 
 		// logs
-		$sql = 'UPDATE ' . LOG_TABLE . "
-			SET log_ip = '127.0.0.1'
-			WHERE log_time < " . (int)$time_run;
+		$sql = 'UPDATE ' . LOG_TABLE . '
+			SET log_ip = "127.0.0.1"
+			WHERE log_time < ' . (int)$time_run . ' AND log_ip <> "127.0.0.1"';
 		$db->sql_query_limit($sql, 200);
 
 		// polls
-		$sql = 'UPDATE ' . POLL_VOTES_TABLE . "
-			SET vote_user_ip = '127.0.0.1'";
+		$sql = 'UPDATE ' . POLL_VOTES_TABLE . '
+			SET vote_user_ip = "127.0.0.1"
+			WHERE vote_user_ip <> "127.0.0.1"';
 		$db->sql_query_limit($sql, 200);
 
 		// sessions
-		$sql = 'UPDATE ' . SESSIONS_TABLE . "
-			SET session_ip = '127.0.0.1'
-			WHERE session_time < " . (int)$time_run;
+		$sql = 'UPDATE ' . SESSIONS_TABLE . '
+			SET session_ip = "127.0.0.1"
+			WHERE session_time < ' . (int)$time_run . ' AND session_ip <> "127.0.0.1"';
 		$db->sql_query_limit($sql, 200);
 
-		$sql = 'UPDATE ' . SESSIONS_KEYS_TABLE . "
-			SET last_ip = '127.0.0.1'";
+		$sql = 'UPDATE ' . SESSIONS_KEYS_TABLE . '
+			SET last_ip = "127.0.0.1"
+			WHERE last_ip <> "127.0.0.1"';
 		$db->sql_query($sql);
 	}
 }
