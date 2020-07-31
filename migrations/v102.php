25d0a3b6d4f4b8ce15a1c522d3c37e3c5f64c8c7
Christian Schnegelberger <mail@crizzo.de>
2020-07-31 18:33:26 +0200

    Add migration for new stats page

---
+++ migrations/v102.php
@@ -0,0 +1,36 @@
+<?php
+/**
+*
+* IP anonymised extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2020 Crizzo <https://www.crizzo.de>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace crizzo\ipanonym\migrations;
+
+class v102 extends \phpbb\db\migration\migration
+{
+	static public function depends_on()
+	{
+		return array(
+			'\crizzo\ipanonym\migrations\init_install',
+		);
+	}
+
+	public function update_data()
+	{
+		$data = array(
+				// Add ACP module
+				array('module.add', array(
+					'acp',
+					'ACP_IP_ANONYM_TITLE',
+					array(
+						'module_basename'	=> '\crizzo\ipanonym\acp\ipanonym_stats',
+						'modes'				=> array('ipanonym_stats'),
+					),
+				)),
+		);
+	}
+}
